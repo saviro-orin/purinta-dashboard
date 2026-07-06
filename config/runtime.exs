2f102c4cb8065bf738ef8f4316c6046fd46d76f1
Saviro Orin <saviro-orin@users.noreply.github.com>
2026-07-06 19:14:35 +0200

    fix: allow local http release startup

--- config/runtime.exs
+++ config/runtime.exs
@@ -19,16 +19,22 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST", "localhost")
   port = String.to_integer(System.get_env("PORT", "4000"))
   scheme = System.get_env("PHX_SCHEME", "http")
+  url_port = String.to_integer(System.get_env("PHX_URL_PORT", Integer.to_string(port)))
 
   config :purinta_dashboard, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
-  config :purinta_dashboard, PurintaDashboardWeb.Endpoint,
-    url: [
-      host: host,
-      port: String.to_integer(System.get_env("PHX_URL_PORT", Integer.to_string(port))),
-      scheme: scheme
-    ],
+  endpoint_config = [
+    url: [host: host, port: url_port, scheme: scheme],
     http: [ip: {0, 0, 0, 0}, port: port],
-    secret_key_base: get_env!.("SECRET_KEY_BASE"),
-    force_ssl: System.get_env("FORCE_SSL") in ~w(true 1)
+    secret_key_base: get_env!.("SECRET_KEY_BASE")
+  ]
+
+  endpoint_config =
+    if System.get_env("FORCE_SSL") in ~w(true 1) do
+      Keyword.put(endpoint_config, :force_ssl, rewrite_on: [:x_forwarded_proto])
+    else
+      endpoint_config
+    end
+
+  config :purinta_dashboard, PurintaDashboardWeb.Endpoint, endpoint_config
 end
